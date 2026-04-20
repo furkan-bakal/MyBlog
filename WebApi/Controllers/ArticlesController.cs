bd6d4969c34a9d42369b362c90b7d07afe218362
furkanbakal <f.bakal1997@gmail.com>
2026-04-20 20:12:12 +0300

    initialize

---
+++ WebApi/Controllers/ArticlesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticlesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new[] { "Article 1", "Article 2", "Article 3" });
+        }
+    }
+}
---
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+/*
+ * This file is part of the WebApi project.
+ * 
+ * (c) 2024 Your Name <
+ * Controllere unit test ve integration test yazýlmaz. Sadece end to end test yapýlabilir.
+ */
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
